8135a896b0550b129ae33d22c6aa6ca1f6becc5f
Hassan Zaki <the.pythonic@gmail.com>
2013-10-24 15:51:28 +0200

    remove puts

--- app/models/bookings/template_slot.rb
+++ app/models/bookings/template_slot.rb
@@ -11,9 +11,6 @@ module Bookings
     	# TODO HZ: reservable.time_slots.where
     	results = Bookings::TemplateSlot.where("(from_time < ? AND to_time > ?) OR (from_time < ? AND to_time > ?)", from_time, from_time, to_time, to_time)
     	results = results.where('day = ?', day)
-    	puts "s"*30
-    	puts results.map(&:id)
-    	puts "a"*30
     	results = results.where("id != ?",  id) unless new_record?
     	errors.add(:from_date, "Can't overlap another slot") if results.exists?
     end
