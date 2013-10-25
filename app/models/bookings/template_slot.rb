e8b99926d8d9c8278345b31a42eaf228188e1fa2
Hassan Zaki <the.pythonic@gmail.com>
2013-10-25 14:24:01 +0200

    merge template slots

--- app/models/bookings/template_slot.rb
+++ app/models/bookings/template_slot.rb
@@ -8,6 +8,8 @@ module Bookings
     validate :no_intersection
     validate :from_less_than_to
 
+    after_save :merge_template_slots
+
     def no_intersection
     	# TODO HZ: reservable.time_slots.where
     	results = Bookings::TemplateSlot.where("(from_time >= :from_time AND from_time < :to_time) OR
@@ -21,6 +23,16 @@ module Bookings
       if from_time && to_time && from_time >= to_time
         self.errors.add(:to_time, "should be after from_time.")
         return false
+      end
+    end
+
+    def merge_template_slots
+      # TODO HZ: reservable.time_slots.where
+      puts  "S" * 50
+      results = Bookings::TemplateSlot.where('day = :day AND (from_time = :to_time OR to_time = :from_time)', {day: day, to_time: to_time, from_time: from_time})
+      puts results
+      unless results.empty?
+
       end
     end
 
