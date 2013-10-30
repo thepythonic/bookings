319cb3a9ffab3b755779fa15740cc44171625121
Hassan Zaki <the.pythonic@gmail.com>
2013-10-30 09:02:25 +0200

    create a helper to render reservable links

--- app/helpers/bookings/appointments_helper.rb
+++ app/helpers/bookings/appointments_helper.rb
@@ -1,4 +1,11 @@
 module Bookings
   module AppointmentsHelper
+  
+  	def reservables_links
+  		links = @reservables.collect do |reservable|
+							 link_to reservable.email, reservable_appointments_path(reservable), class: ("current" if reservable == @reservable)
+							end
+  		raw links.join(' | ')
+  	end
   end
 end
