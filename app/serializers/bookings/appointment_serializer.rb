444873df6c23ed4604f78d8515d53ca460cb855a
Hassan Zaki <the.pythonic@gmail.com>
2013-10-31 12:12:30 +0200

    refactor serializer, add new field time_slot to distinguish between appointment and time slot

--- app/serializers/bookings/appointment_serializer.rb
+++ app/serializers/bookings/appointment_serializer.rb
@@ -1,7 +1,7 @@
 module Bookings
   class AppointmentSerializer < ActiveModel::Serializer
-    attributes :id, :start, :end, :title, :recurring
-    #TODO HZ: refactor, add attribute time_slot
+    attributes :id, :start, :end, :title, :recurring, :time_slot
+
     def recurring
       begin
         object.recurring
@@ -10,43 +10,24 @@ module Bookings
       end
     end
 
-    #TODO HZ: get start_day from configuration
-    def beginning_of_this_week
-      DateTime.now.beginning_of_week(start_day= :sunday)
-    end
-
-    def week_days
-      DateTime::DAYNAMES
+    def time_slot
+      begin
+        true if object.recurring
+      rescue 
+        false
+      end
     end
 
     def start
-      begin
-        object.recurring
-        h_m = object.to_time.split(':')
-        format_date h_m
-      rescue
-        object.from_time
-      end
+      object.from_time
     end
 
     def end
-      begin
-        object.recurring
-        h_m = object.to_time.split(':')
-        format_date h_m
-      rescue
-        object.to_time
-      end
+      object.to_time
     end
 
     def title
       object.id
-    end
-
-    private
-    def format_date(hour_minute)
-      day = beginning_of_this_week
-      day + week_days.index(object.day) + hour_minute[0].to_i.hour + hour_minute[1].to_i.minute
     end
   end
 end
