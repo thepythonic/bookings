99280f10dd3740386afab8a1589ae0b351a5b275
Hassan Zaki <the.pythonic@gmail.com>
2013-10-31 14:20:12 +0200

    make recurring default = 1

--- db/migrate/20131022162450_create_bookings_time_slots.rb
+++ db/migrate/20131022162450_create_bookings_time_slots.rb
@@ -3,7 +3,7 @@ class CreateBookingsTimeSlots < ActiveRecord::Migration
     create_table :bookings_time_slots do |t|
       t.datetime :from_time
       t.datetime :to_time
-      t.integer :recurring
+      t.integer :recurring, default: 1
       
       t.integer :reservable_id, index: true
       t.integer :parent_id, index: true
