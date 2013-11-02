83e61499f000f6b5508845ad97779fee31f8b9eb
Hassan Zaki <the.pythonic@gmail.com>
2013-11-02 21:33:38 +0200

    add time slot spec

---
+++ test/dummy/spec/models/time_slot_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe Bookings::TimeSlot do
+  
+  describe 'Reservable' do
+
+    it "should be able to create recurring time slots" do
+    end
+
+    it "should be able to update time slot with same recurring" do
+    end
+
+    it "should be able to update time slot and increase recurring" do
+    end
+
+    it "should be able to update time slot and decrease recurring" do
+    end
+
+    it "should not be able to create time slot that intersects with another time slot" do
+    end
+
+    it "should not be able to update time slot to intersects with another time slot" do
+    end
+
+  end
+
+  describe 'Admin' do
+  end
+
+end
