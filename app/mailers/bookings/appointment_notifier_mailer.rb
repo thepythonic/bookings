47e93660c0c9de4f59503d34795441e290051863
Hassan Zaki <the.pythonic@gmail.com>
2013-11-04 11:22:42 +0200

    code refacotr, and small fix

--- app/mailers/bookings/appointment_notifier_mailer.rb
+++ app/mailers/bookings/appointment_notifier_mailer.rb
@@ -6,14 +6,11 @@ module Bookings
     	@user = user
     	@appointment = appointment
     	@old_appointment = old_appointment
-    	puts @old_appointment.from_time
-    	puts @appointment.from_time
-    	puts "S" * 120
     	to = if user.is_admin?
     		[appointment.reservable.email, appointment.customer.email]
     	elsif user.is_customer?
     		appointment.reservable.email
-    	elsif appointment.is_reservable?
+    	elsif user.is_reservable?
     		appointment.customer.email
     	end
     	mail(to: to, subject: "Appointment is Rescheduled")
