0488c0b0936d34d4984479987aa37f17e09d6e58
Hassan Zaki <the.pythonic@gmail.com>
2013-11-06 09:53:29 +0200

    code refactor

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -5,27 +5,29 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # TODO HZ: provided by user
-  def is_admin?
-    self.admin
-  end
-  def can_find_customers?
-    self.admin
-  end
-
-  # TODO HZ: provided by user
   def is_reservable?
-  	if self.is_a? Patient
-  		return false
-  	end
-  	true
+    if self.is_a? Patient
+      return false
+    end
+    true
   end
 
   # TODO HZ: provided by user
   def is_customer?
-  	if self.is_a? Patient
-  		return true
-  	end
-  	false
+    if self.is_a? Patient
+      return true
+    end
+    false
+  end
+
+  # TODO HZ: provided by user
+  def is_admin?
+    self.admin
+  end
+
+  # TODO HZ: provided by user
+  def can_find_customers?
+    self.admin
   end
 
   # TODO HZ: provided by user
